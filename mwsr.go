1e0825849a7efb9b5ff531d5d87af7c26da1d725
Mark Karpeles <magicaltux@gmail.com>
2020-05-13 01:03:13 +0900

    simplify code

--- mwsr.go
+++ mwsr.go
@@ -37,59 +37,50 @@ func (m *Mwsr) Write(v interface{}) error {
 	m.lk.RLock()
 	// we do not defer RUnlock() because we may switch to a write lock during the process
 
-	for {
-		if m.err != nil {
-			// if m.err is set, it won't be modified, so thread safety is fine
-			m.lk.RUnlock()
-			return m.err
-		}
+	if m.err != nil {
+		// if m.err is set, it won't be modified, so thread safety is fine
+		m.lk.RUnlock()
+		return m.err
+	}
 
-		pos := atomic.AddUint32(&m.pos, 1) - 1
-		if pos >= uint32(len(m.obj)) {
-			// too full, attempt to process now
-			m.lk.RUnlock()
-
-			// switch to full lock
-			m.lk.Lock()
-
-			// an error occured since the lock, return it
-			if m.err != nil {
-				m.lk.Unlock()
-				return m.err
-			}
-
-			if m.pos < uint32(len(m.obj)) {
-				// looks like someone else flushed the queue while we acquired the lock.
-				// avoid running the callback now as we don't need to
-				m.obj[m.pos] = v
-				m.pos += 1 // no need for atomic since we hold the write lock
-
-				m.cd.Broadcast()
-				m.lk.Unlock()
-				return nil
-			}
-
-			if err := m.doCallback(); err != nil {
-				// an error occured, give up here
-				m.err = err
-				m.lk.Unlock()
-				return err
-			}
-
-			// at this point, the queue is supposed to be empty
-			m.obj[m.pos] = v
-			m.pos += 1
-
-			m.cd.Broadcast()
-			m.lk.Unlock()
-			return nil
-		}
-
+	// generate new atomic value, check if it fits
+	pos := atomic.AddUint32(&m.pos, 1) - 1
+	if pos < uint32(len(m.obj)) {
+		// it fits, store and return
 		m.obj[pos] = v
 		m.cd.Broadcast()
 		m.lk.RUnlock()
 		return nil
 	}
+
+	// too full, attempt to process now. First, let's switch to write lock
+	m.lk.RUnlock()
+	m.lk.Lock()
+
+	// did an error occur between the unlock/lock? return it
+	if m.err != nil {
+		m.lk.Unlock()
+		return m.err
+	}
+
+	// make sure we still don't have room in the queue (might have emptied between the locks)
+	if m.pos >= uint32(len(m.obj)) {
+		// the queue is indeed full, flush it now
+		if err := m.doCallback(); err != nil {
+			// an error occured, give up here
+			m.err = err
+			m.lk.Unlock()
+			return err
+		}
+	}
+
+	// add value to the queue now that there is room
+	m.obj[m.pos] = v
+	m.pos += 1 // no need for atomic since we hold the write lock
+
+	m.cd.Broadcast()
+	m.lk.Unlock()
+	return nil
 }
 
 // Flush will force the queue to be flushed now, and return once the queue is
